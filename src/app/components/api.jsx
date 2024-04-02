69b3828f73acb8990141c5e5f2cf211403ce1a53
Rhys Postans <rmp71171@icloud.com>
2024-04-02 17:57:40 +0100

    creating api calls

--- src/app/components/api.jsx
+++ src/app/components/api.jsx
@@ -25,15 +25,22 @@ export async function getDictionaryData(randomWord){
         if (!response.ok) throw new Error(`Failed to fetch the dictionary API for word: ${randomWord}`)
         const data = await response.json();
         if (!data[0] || !data[0].phonetics || !data[0].phonetics.length) {              //<=assess if all 'if' parameters are required
-            throw new Error("Phonetic data is missing or empty.");
+            throw new Error("Phonetic data is missing or empty...");
         }
-    
+        //Find phonetic
+        const phonetic = data[0].phonetic.find(p => p.text)?.text;
+        if (!phonetic) {
+            throw new Error ("Phonetic not found...")
+        }
+
+        //Find definition - iterates over all keys called "meanings" to find meanings.definitions and then maps over meanings.definitions to find and create an array of all meanings.definitions.definition. 
+        const definitions = data[0].meanings.map(meaning => meaning.definitions.map(def.def.definition)).flat();
 
 
     const dictionaryData = {            // destructure in future
         word: randomWord,
         phonetic: phonetic,
-        definition: definition,
+        definition: definitions,
     }
 
 
