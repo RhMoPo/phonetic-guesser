f054500a34f382dc543d93fdf58e817aaf491eb4
Gavin-Persad <gavinapersad@gmail.com>
2024-04-02 17:34:50 +0100

    working on dictionary call function

--- src/app/components/api.jsx
+++ src/app/components/api.jsx
@@ -2,19 +2,40 @@
 //F: random word api call 
 //F: use random word in dictionary API call
 // return Word, Phonetic and definition inside obejct
-export default async function getRandomWord() {
+export async function getRandomWord() {
     try {
-        const response = await fetch("https://random-word-api.herokuapp.com/word")
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+        const randomWordResponse = await fetch("https://random-word-api.herokuapp.com/word")
+        if (!randomWordResponse.ok) {
+            throw new Error(`HTTP error! status: ${randomWordResponse.status}`);
         }
-        let data = await response.json();
+        let data = await randomWordResponse.json();
         console.log("🚀 ~ getRandomWord ~ data:", data)
         let randomWord = data[0]
         console.log("🚀 ~ getRandomWord ~ randomWord:", randomWord)
-        await getDeictionaryData(randomWord)
+        await getDictionaryData(randomWord)
     } catch (error) {
         console.error("Error:", error.message)
     }
 }
 //export default dataFetch
+
+export async function getDictionaryData(randomWord){
+    try{
+        const response = await fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`);
+        if (!response.ok) throw new Error(`Failed to fetch the dictionary API for word: ${randomWord}`)
+        const data = await response.json();
+        if (!data[0] || !data[0].phonetics || !data[0].phonetics.length) {              //<=assess if all 'if' parameters are required
+            throw new Error("Phonetic data is missing or empty.");
+        }
+    
+
+
+    const dictionaryData = {            // destructure in future
+        word: randomWord,
+        phonetic: phonetic,
+        definition: definition,
+    }
+
+
+}
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,26 +4,17 @@ import styles from "./page.module.css";
 import "./globals.css"
 import React, { useState, useEffect } from "react";
 
-export default function Game() {
+export default function Home() {
 
-    useEffect(() => {
-        getRandomWord();
-    }, [])
-    //Use effect with empty depednecy - works once on page load
-    //Initial API call to random word api call/function - imported from API file
-    // save imported api data in state
-    
-
-    
  return(
     <div className="container">
        <img className="exampleImg" src="example.png" alt="Example"></img>
        <div className="inputSection">
-        <form className="inputForm">
+        <form className="userInput">
             <input type="text" id="userAnswer" placeholder="Use the phonetic to guess the word..." />
             <button id="submitBTN">Guess</button>
         </form>
-          
+          <button id="hintBTN" style={{ marginLeft: '10px' }}>Hint?</button>
        </div>
    </div>
  )
